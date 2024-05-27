af31491322aa61dbf095f8572e1379c60e036449
Kieran <kieran@mirasaki.dev>
2024-05-27 17:33:16 +0100

    Updated latest deploy workflow

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/breath/repos/reforger-whitelist-py/venv"
+set -gx VIRTUAL_ENV "/home/kieran/repos/ReforgerWhitelistPy/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
