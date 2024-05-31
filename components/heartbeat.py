71ddaba0f644fe8fc6667394ee240ca1da6099e7
Kieran <kieran@mirasaki.dev>
2024-06-01 00:06:11 +0100

    Added heartbeat to components

---
+++ components/heartbeat.py
@@ -0,0 +1,18 @@
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+def heartbeat(count: int) -> None:
+    """Sends log and CLI messages every count in seconds.
+
+    ...
+
+    Parameters
+    ----------
+    count : int
+        Interval between messages.
+    """    
+    while True:
+        logger.info("Whitelist is running... Use [Ctrl + C] to stop the application.")
+        time.sleep(count)
