12c8f603c91890792b4f463eab7723cac5c2f80a
Kieran <kieran@mirasaki.dev>
2024-05-31 23:56:43 +0100

    Added check player component

---
+++ components/check_player.py
@@ -0,0 +1,108 @@
+import sqlite3
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+def is_player_in_database(player_name: str, identity_id: str, db_path: str) -> bool:
+    """Checks if a player's identifier is in the database.
+
+    ...
+
+    Parameters
+    ----------
+    player_name : str
+        The player's gamertag (Console) or name (PC).
+    identity_id : str
+        The player's GUID (BID).
+    db_path : str
+        The system path to the database file.
+
+    Returns
+    ----------
+    bool
+        Whether the player is in the database (True) or not (False).
+    """
+    try:
+        with sqlite3.connect(db_path) as conn:
+            cur = conn.cursor()
+            cur.execute(
+                """
+                SELECT whitelisted 
+                FROM user_data 
+                WHERE (LOWER(game_name) = LOWER(?) OR LOWER(game_name) = LOWER(?)) 
+                AND whitelisted = 1
+                """,
+                (player_name, identity_id),
+            )
+            is_whitelisted = cur.fetchone()
+            if is_whitelisted is not None:
+                logger.info(
+                    "Player %s or IdentityId %s found in database and is whitelisted."
+                    % (
+                        player_name,
+                        identity_id,
+                    )
+                )
+                return True
+            else:
+                logger.info(
+                    "Player %s or IdentityId %s not found in database or not whitelisted."
+                    % (
+                        player_name,
+                        identity_id,
+                    )
+                )
+                return False
+    except sqlite3.Error as database_error:
+        logger.error("Database error: %s" % database_error)
+        return False
+
+
+def is_player_in_json(player_name: str, identity_id: str, json_path: str) -> bool:
+    """Checks if the player is in the JSON.
+
+    ...
+
+    Parameters
+    ----------
+    player_name : str
+        The player's gamertag (Console) or name (PC).
+    identity_id : str
+        The player's GUID (BID).
+    json_path : str
+        The system path to the JSON file.
+
+    Returns
+    ----------
+    bool
+        Whether the player is in the database (True) or not (False).
+    """    
+    try:
+        with open(json_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            for player in data.get("players", []):
+                if (
+                    player_name.lower() == player.get("game_name", "").lower()
+                    or identity_id.lower() == player.get("identity_id", "").lower()
+                ):
+                    if player.get("whitelisted", 0) == 1:
+                        logger.info(
+                            "Player %s or IdentityId %s found in JSON and is whitelisted."
+                            % (
+                                player_name,
+                                identity_id,
+                            )
+                        )
+                        return True
+            logger.info(
+                "Player %s or IdentityId %s not found in JSON or not whitelisted."
+                % (
+                    player_name,
+                    identity_id,
+                )
+            )
+            return False
+    except json.JSONDecodeError as json_error:
+        logger.error("JSON error: %s" % json_error)
+        return False
