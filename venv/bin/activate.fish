6464067b666890ec5f7a7d3e954eeaea35f659ae
Kieran Lane <kieranlane@mac.home>
2025-05-03 21:03:16 +0100

    Updated requirements and removed venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -13,17 +13,13 @@ function deactivate  -d "Exit virtual environment and return to normal shell env
     end
 
     if test -n "$_OLD_FISH_PROMPT_OVERRIDE"
+        functions -e fish_prompt
         set -e _OLD_FISH_PROMPT_OVERRIDE
-        # prevents error when using nested fish instances (Issue #93858)
-        if functions -q _old_fish_prompt
-            functions -e fish_prompt
-            functions -c _old_fish_prompt fish_prompt
-            functions -e _old_fish_prompt
-        end
+        functions -c _old_fish_prompt fish_prompt
+        functions -e _old_fish_prompt
     end
 
     set -e VIRTUAL_ENV
-    set -e VIRTUAL_ENV_PROMPT
     if test "$argv[1]" != "nondestructive"
         # Self-destruct!
         functions -e deactivate
@@ -33,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/breath/repos/reforger-whitelist-py/venv"
+set -gx VIRTUAL_ENV "/Users/kieranlane/Documents/ReforgerWhitelistPy/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -65,5 +61,4 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(venv) "
 end
